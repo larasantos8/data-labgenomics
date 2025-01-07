4c81d32987726d5f154b4dac4e56f2fc36085b5d
Lara Santos <lara.dossantos_dextra@prestador.globo>
2025-01-06 22:20:32 -0300

    convert type file

--- about.js
+++
@@ -1,9 +0,0 @@
-export const about = {
-    image: '',
-    title: 'Laboratory of evolutionary genomics',
-    contact: {
-        github: 'https://github.com/EvolutionaryGenomicsLab',
-        email: 'neryslge@gmail.com'
-    },
-    description: 'Our group investigates the genomic bases of phenotypic diversity and the role of evolutionary forces in generating this diversity, from population-level to higher taxonomic levels. Our research program outlines broad questions about the emergence of new groups and the mechanisms underlying evolutionary innovations and biodiversity generation. We also seek to answer questions about convergent evolution and the extent to which evolution uses the same pathways or molecular bases to arrive at solutions to similar problems. In other words, how creative can evolution be? Specifically, we explore how evolution has occurred at the molecular level, affecting coding and non-coding regions in species genomes. We also investigate how the dynamics of gain and loss of genes in certain gene families affect species adaptation in their evolutionary trajectories. What is the evolutionary potential of genomes in the face of global changes we are experiencing today? Our projects do not focus on a specific model group: we mainly work with mammals (especially cetaceans), but also with other vertebrates, plants, and bacteria. Our research always occurs within a phylogenetic context, and we also perform phylogenetic reconstructions using genomic data to answer our scientific questions.'
-}
