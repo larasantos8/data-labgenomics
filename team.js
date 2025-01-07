28dd1719de77c062a24b4da29dd7da65211cf36f
Lara Santos <lara.dossantos_dextra@prestador.globo>
2025-01-06 22:01:17 -0300

    add json files

---
+++ team.js
@@ -0,0 +1,160 @@
+export const team = {
+    principal: {
+        description: "I have always been fascinated by whales and dolphins, and they have been my study model since my master's degree. Currently, I work with comparative genomics, focusing on aquatic mammals, but also interested in other groups that have occupied new environments in their evolutionary histories. My research aims to understand the evolution of species from a molecular point of view, functional and structurally. From micro to (really) macro, I still think that whales are the best model to study evolution. I have been leading the Evolutionary Genomics Laboratory at Unicamp since 2015, and I am a mother of four children. In my almost non-existent free time, I enjoy reading and doing puzzles.",
+        image: '/assets/images/team/marianaNery.png',
+        name: 'Mariana Freitas Nery',
+        email: 'marinery@unicamp.br',
+        site: 'http://lattes.cnpq.br/1410858776932991',
+    },
+    posdoc_fellow: [
+      {
+        description: "I have always been passionate about animals and have been studying spiders since 2002. I hold a master's and a Ph.D. in Zoology from USP, and I was part of the Arachnology Lab at California Academy of Sciences as the Schlinger Chair of Arachnology Postdoctoral Fellow. My research focuses on the systematics and evolution of spiders, and currently, I am seeking to understand the morphological and molecular basis of leg loss in this group. I have two children and I am also one of the founders of the Support Women in Arachnology (SWA) collective, which aims to promote gender equity in arachnology. In my free time, I pratice pole dancing, circus arts, and play the violin.",
+        image: '/assets/images/team/linaAlmeida.jpg',
+        name: 'Lina Maria Almeida-Silva',
+        email: 'linamas@gmail.com',
+        site: 'http://lattes.cnpq.br/7440874734504294',
+      },    
+    ],
+    phd: [
+      {
+        description: "I’m a PhD student in Genetics and Molecular Biology at the University of Campinas, working at the Laboratory of Evolutionary Genetics (LGE), where I also earned my Master's degree. I began my academic journey with a Bachelor's degree in Biological Sciences from the Federal University of Alfenas. My current research explores evolutionary adaptations in genes related to energy metabolism in aquatic mammals. Since a young age, I have been fascinated by the fields of evolution and genetics, and my passion for these subjects has only grown stronger over time. In addition to my academic pursuits, I am deeply interested in music and pop culture, and I love being around nature.",
+        image: '/assets/images/team/beatrizdaros.png',
+        name: 'Beatriz Daros',
+        email: 'beatrizdarosbio@gmail.com',
+        site: 'http://lattes.cnpq.br/7161505800252714',
+      },
+      {
+        description: "I'm a PhD student at LGE, and I've been here since my scientific initiation during my graduation at UNICAMP. I've always been passionate about the sea and aquatic mammals, which is why I entered the laboratory, but my interest in evolution ultimately won me over. My research focuses on the molecular evolution of the antioxidant enzyme system. When I'm not in the lab, I enjoy practicing sports, particularly kung fu.",
+        image: '/assets/images/team/giovannaSelleghinVeiga.png',
+        name: 'Giovanna Selleghin-Veiga',
+        email: 'giovanna.slvg@gmail.com',
+        site: 'http://lattes.cnpq.br/7345320897514459',
+      },    
+      {
+        description: "Hello! I am a PhD student in Prof. Nery's laboratory here at University of Campinas, in the Genetics and Molecular Biology program. I am studying transposable elements in cetacean genomes and their putative contribution to the molecular evolution of this lineage. I have a Master's degree in Cellular and Molecular Biology and a bachelor's degree in Biological Sciences from São Paulo State University at Rio Claro. I'm interested in evolution, data science, and science education. Outside the lab, I try to balance baseball, jiu-jitsu, the gym, financial debt, and contemporary Brazil in some conscious rap with a 90s trip-hop beat.",
+        image: '/assets/images/team/lucasAlbuquerque.png',
+        name: 'Lucas Albuquerque dos Santos',
+        email: 'lucasabq@live.com',
+        site: 'http://lattes.cnpq.br/5420552519432124',
+      },
+      {
+        description: "PhD student in the Genetics and Molecular Biology program. Here at LGE, I study aquatic mammals limb development and evolution. I hold a bachelor's degree in Biological Sciences from Universidade Federal da Bahia (UFBA), where I started studying EvoDevo. In my free time, I enjoy coffees and restaurants, reading and taking good naps!",
+        image: '/assets/images/team/nicolleLeto.png',
+        name: 'Nicolle Leto',
+        email: 'letonicolle@gmail.com ',
+        site: 'http://lattes.cnpq.br/5529567731673644',
+      },
+    ],
+    masters: [
+      {
+        description: "I am a Master’s student in the Genetics and Molecular Biology graduate program at the University of Campinas and I have a bachelor in Biological Sciences from Paulista’s State University (Bauru campus). It is of great interest to me to study the evolution of animals and my research is focused on the molecular evolution of the sensory systems of cetaceans and sirenians. I love music, musicals and cooking delicious meals.",
+        image: '/assets/images/team/analuiza.png',
+        name: 'Ana Luiza Borba',
+        email: 'analuizaclein@gmail.com',
+        site: 'http://lattes.cnpq.br/1921686981791604',
+      },
+      {
+        description: "Master's student in Genetics and Molecular Biology at the State University of Campinas and I graduated in Biology from the Pontifical Javeriana University in Colombia. My research project aims to study the evolutionary history of river dolphins, considering the transition from marine to freshwater environments. I am passionate about 80s rock music and discovering new places.",
+        image: '/assets/images/team/danielpinilla.png',
+        name: 'Daniel Pinilla',
+        email: 'daniel.pinilla1997@gmail.com',
+        site: 'http://lattes.cnpq.br/4609223114562711',
+      },
+      {
+        description: "I hold a Bachelor's degree in Biology from the Universidade do Estado de Minas Gerais (UEMG) and am currently a Master's student in the Genetics and Molecular Biology Program at the Universidade Estadual de Campinas (UNICAMP). My fields of interest in Biology have always been diverse, but currently I've been studying the molecular and morphological evolution of the pelvic girdle in cetaceans. Outside the labs, my main interests include books, music, coffee, and taking care of my fur babies (four cats and one elderly dog).",
+        image: '',
+        name: 'Leonardo Duarte Santos',
+        email: 'leonardods0000@gmail.com',
+        site: 'http://lattes.cnpq.br/1358449276845228',
+      },
+      {
+        description: "Master’s student of the Graduate Program in Genetics and Molecular Biology (Unicamp - SP) and Bachelor in Biological Sciences (Unifal - MG), I am interested chiefly in evolutionary scenarios and their stories. Hence, my research focuses on understanding whether the social complexity seen in cetaceans may be the main selective pressure for the evolution of these mammal’s brains. Aside from the academic scene, but not university life, parties and convos suit my routine.",
+        image: '/assets/images/team/marianaMelo.png',
+        name: 'Mariana Melo',
+        email: 'm243223@dac.unicamp.br',
+        site: 'http://lattes.cnpq.br/9143556136283260',
+      },
+    ],
+    undergrad: [
+      {
+        description: "I am a Biology undergraduate student at the University of Campinas. I have a degree in Science, Literature, and Arts from ProFIS. I am doing my Scientific Initiation at LGE, however, I do not work with genetics, I prefer to work with Zoology and Natural History. My research project aims to investigate sexual segregation in cetaceans. I love the beach, watching cartoons, or staying with my pets.",
+        image: '/assets/images/team/brunamarconato.png',
+        name: 'Bruna Marconato',
+        email: 'brunamarconato1@gmail.com',
+        site: 'http://lattes.cnpq.br/4190028544313462',
+      },
+      {
+        description: "As an undergraduate student majoring in Biological Science at the Institute of Biosciences of UNESP - Botucatu, I am passionate about understanding how evolutionary processes can overcome complex challenges in various contexts. At present, my research project centers on examining the evolution of regulatory regions in cancer-associated bat genes. When I am not in the lab, I enjoy spending my free time engaging in hobbies such as tinkering with my 3D printer, playing tabletop RPGs, and exploring different aspects of “nerd” and “maker” cultures.",
+        image: '/assets/images/team/mathiasRennoChaves.png',
+        name: 'Mathias Rennó Chaves',
+        email: 'mathiasrennochaves@gmail.com',
+        site: 'http://lattes.cnpq.br/4451643961759570',
+      }
+    ],
+    memoriam: [
+      {
+        description: "JP was a scientific initiation student at LGE, where he researched the molecular evolution of genes related to blood coagulation, focusing on fish and cetaceans. The published paper from his scientific initiation project, entitled 'Unraveling the Molecular Evolution of Blood Coagulation Genes in Fishes and Cetaceans,' was elected as the best in the Biodiversity field at the Institute of Biology (IB). João Pedro was beloved by everyone during his time in the laboratory, and we were thrilled when he went on to pursue a Master's degree in Japan, fulfilling a dream. Unfortunately, João Pedro left us prematurely, leaving behind a void and many fond memories. Today, JP's memory lives on at IB through the 'João Pedro Mariz' Award for Outstanding Scientific Article of the Year at IB.",
+        image: '',
+        name: 'João Pedro Mariz (JP)',
+        email: 'email@gmail.com',
+        site: 'www.google.com',
+      }
+    ],
+    collaborators: [
+      {
+        name: 'Flávia Akemi Nitta Fernandes',
+        degree: 'Master’s degree',
+        site: 'http://lattes.cnpq.br/4326910987159123'
+      },
+      {
+        name: 'Erica Martinha Silva de Souza',
+        degree: 'Ph.D degree',
+        site: 'http://lattes.cnpq.br/9530933208734813'
+      },
+      {
+        name: 'Maria Luiza Andreani',
+        degree: 'Master’s degree',
+        site: '​​http://lattes.cnpq.br/1031570416362681'
+      },
+      {
+        name: 'Bruna Cristina Dias',
+        degree: 'Master’s degree',
+        site: 'http://lattes.cnpq.br/7525609810215272'
+      },
+      {
+        name: 'Felipe André Silva',
+        degree: 'Master’s degree',
+        site: 'http://lattes.cnpq.br/5292077779972313'
+      },
+      {
+        name: 'Letícia Magpali Moura Estevão',
+        degree: 'Undergrad Research and collaborator',
+        site: 'http://lattes.cnpq.br/2690712248724754'
+      },
+      {
+        name: 'Michelle Carneiro Rachid Ribeiro',
+        degree: 'Undergrad Research',
+        site: 'http://lattes.cnpq.br/7619019791980676'
+      },
+      {
+        name: 'Agnello César Rios Picorelli',
+        degree: 'Collaborator',
+        site: 'http://lattes.cnpq.br/3037646281651484'
+      },
+      {
+        name: 'Elisa Karen da Silva Ramos',
+        degree: 'Ph.D degree',
+        site: 'http://lattes.cnpq.br/7174591567628859'
+      },
+      {
+        name: 'Lucas Araújo Freitas',
+        degree: 'Pos-doc',
+        site: 'http://lattes.cnpq.br/7158471076088943'
+      },
+      {
+        name: 'Marcos Adriano Vaz Barbosa',
+        degree: 'Undergrad Research',
+        site: 'http://lattes.cnpq.br/3447371090950093'
+      },
+    ],
+}
